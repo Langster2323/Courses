d108b0c794cfee120f8f107da3f494b2178e880d
Ahkeem Lang <langahkeem@gmail.com>
2017-09-20 14:16:00 -0500

    Changed a note because my previous note was incorrect

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => (
   //root router that listens to URL changes
   <BrowserRouter>
     <div className="container">
-    //Render components via the route user defined component
+    //React doesn't render any of the routing components to the DOM. They just manage what's being rendered
     <Header />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
