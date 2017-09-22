4e217cb5e8caf7d5a5025178d7580a20fb84043e
Ahkeem Lang <langahkeem@gmail.com>
2017-09-21 20:25:25 -0500

    Having the navigation connected to the routes I have already declared. So I can interact with the Nav bar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><Link to="#">Home</Link></li>
-      <li><Link to="#">About</Link></li>
-      <li><Link to="#">Teachers</Link></li>
-      <li><Link to="#">Courses</Link></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/About">About</Link></li>
+      <li><Link to="/Teachers">Teachers</Link></li>
+      <li><Link to="/Courses">Courses</Link></li>
     </ul>
   </header>
 );
