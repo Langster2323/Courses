3e3150493cb13ba80224121f17e73ad1cf37ac57
Ahkeem Lang <langahkeem@gmail.com>
2018-07-24 11:11:03 -0500

    Imported navlink into the courses component

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink='/courses/html'>HTML</NavLink></li>
+        <li><NavLink='/courses/css'>CSS</NavLink></li>
+        <li><NavLink='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
-    
+
     {/* Write routes here... */}
   </div>
 );
