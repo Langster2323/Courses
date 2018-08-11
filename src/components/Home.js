629e5aff81e5b3381fefbcf51a1f2cb0c12b432a
Ahkeem Lang <langahkeem@gmail.com>
2018-08-11 08:29:09 -0500

    Passed the handleSubmit event

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ handleSubmit = (e) =>
    let teacherName = this.name.value;
    let teacherTopic = this.topic.value;
    let path = `teachers/${teacherTopic}/${teacherName}`
+   this.props.history.push(path);
  }
   render() {
     return (
@@ -19,7 +20,7 @@ handleSubmit = (e) =>
         <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
         <hr />
         <h3>Featured Teachers</h3>
-        <form>
+        <form onSubmit={this.handleSubmit}>
         <input type="text" placeholder="Name" ref={ (input) => this.name = input }/>
         <input type="text" placeholder="Topic" ref={ (input) => this.topic = input }/>
         <button type="submit">Go!</button>
