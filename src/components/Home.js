825e4a61fbdee66865d9c7e74d5b3d6d494368e5
Langster2323 <langahkeem@gmail.com>
2019-06-26 00:52:31 -0500

    Changed the content

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,10 +14,9 @@ handleSubmit = (e) =>
   render() {
     return (
       <div className="main-content home">
-        <h2>Front End Course Directory</h2>
-        <p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
-        <p>Learn front end web development and much more! This simple directory app offers a preview of our course library. Choose from many hours of content, from HTML to CSS to JavaScript. Learn to code and get the skills you need to launch a new career in front end web development.</p>
-        <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
+        <h2>Student Course Directory</h2>
+        <p>Learn Math and Science and much more! This simple directory app offers a preview of our course library. Choose from many hours of content, from Math to Science to English. Learn to be a great student</p>
+        <p>We have thousands of videos created by expert teachers. Our library is continually refreshed with the latest material so you will never fall behind.</p>
         <hr />
         <h3>Featured Teachers</h3>
         <form onSubmit={this.handleSubmit}>
--- src/data/teachers.js
+++ src/data/teachers.js
@@ -1,39 +1,39 @@
 const TeacherList = [
   {
     name: "Angie McAngular",
-    bio: "Angie is a web developer and teacher who is passionate about building scalable, data driven web apps, especially ones that address old problems with new tech!",
+    bio: "Angie Scientist and teacher who is passionate about animal life!",
     img_src: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/angie.png",
-    id: "teach-1"
+    id: "1"
   },
   {
     name: "NodeStradamus",
-    bio: "'NodeStra' is a software engineer and philosopher trying to leave the world better than he found it. He codes for non-profits, eCommerce, and large-scale web apps.",
+    bio: "'NodeStra' is a english teacher and philosopher trying to leave the world better than he found it.",
     img_src: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/nodestradamus.png",
-    id: "teach-2"
+    id: "2"
   },
   {
     name: "Geo 'Lo' Cation",
-    bio: "Geo is a JavaScript developer working on large-scale applications. He's also a teacher who strives to support students in removing all barriers to learning code.",
+    bio: "Geo is a Math teacher working on large-scale applications. He's also a teacher who strives to support students in removing all barriers to learning math.",
     img_src: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/geo.png",
-    id: "teach-3"
+    id: "3"
   },
   {
     name: "Ecma Scriptnstuff",
-    bio: "Ecma found her passion for computers and programming over 15 years ago. She is excited to introduce people to the wonderful world of JavaScript.",
+    bio: "Ecma found her passion for Chemisty and Biology over 15 years ago. She is excited to introduce people to the wonderful world of bacteria.",
     img_src: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/ecma.png",
-    id: "teach-4"
+    id: "4"
   },
   {
     name: "Jay Query",
-    bio: "Jay is a developer, author of CSS: The Missing Manual, JavaScript & jQuery: The Missing Manual, and web development teacher.",
+    bio: "Jay is a British English teacher, author of British:Found my toe.",
     img_src: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/jay.png",
-    id: "teach-5"
+    id: "5"
   },
   {
     name: "Json Babel",
-    bio: "All of his professional life, Json has worked with computers online; he is a polyglot programmer and likes using the right tools for the job.",
+    bio: "All of his professional life, Json has worked with Trig; he is a polyglot programmer and likes using the right tools for the job.",
     img_src: "http://treehouse-code-samples.s3.amazonaws.com/bootstrap-4/img/json.png",
-    id: "teach-6"
+    id: "6"
   }
 ];
 
