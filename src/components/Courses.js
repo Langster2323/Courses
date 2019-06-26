359c44c1f71858eff32bc5b7112a088633618727
Langster2323 <langahkeem@gmail.com>
2019-06-25 23:02:01 -0500

    Updated courses

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, NavLink, Redirect } from 'react-router-dom';
 
 import CourseContainer from './courses/CourseContainer';
-import { HTMLCourses, CSSCourses, JSCourses } from '../data/courses';
+import { Math, Science, English } from '../data/courses';
 
 
 const Courses = ({match}) => (
@@ -10,17 +10,17 @@ const Courses = ({match}) => (
     <div className="course-header group">
       <h2>Courses</h2>
       <ul className="course-nav">
-        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
-        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
-        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
+        <li><NavLink to={`${match.url}/html`}>MATH</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>SCIENCE</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>ENGLISH</NavLink></li>
       </ul>
     </div>
 
     {/* Write routes here... */}
     <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`}/> } />
-    <Route path={`${match.path}/html`} render={ () => <CourseContainer data={HTMLCourses} /> } />
-    <Route path={`${match.path}/css`} render={() => <CourseContainer data={CSSCourses} />} />
-    <Route path={`${match.path}/javascript`} render={() => <CourseContainer data={JSCourses} />} />
+    <Route path={`${match.path}/html`} render={ () => <CourseContainer data={Math} add={this.handleSubmit} /> } />
+    <Route path={`${match.path}/css`} render={() => <CourseContainer data={Science} add={this.handleSubmit} />} />
+    <Route path={`${match.path}/javascript`} render={() => <CourseContainer data={English} add={this.handleSubmit} />} />
   </div>
 );
 
--- src/components/courses/CourseContainer.js
+++ src/components/courses/CourseContainer.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Course from './Course';
 
-const CourseContainer = (props) => {
-  let courses = props.data.map((course) => {
-    return <Course title={course.title}
-                   desc={course.description}
-                   img={course.img_src}
-                   key={course.id} />
-  });
-  return (
-    <div>
-      <ul>
-        {courses}
-      </ul>
-    </div>
-  );
+class CourseContainer extends Component {
+  handleAddCourse = (title, description) => {
+    this.setState( prevState => {
+      return {
+        data: [
+          ...prevState.data,
+          {
+            title: "",
+            description: ""
+          }
+        ]
+      };
+    });
+  }
+  render() {
+    const {
+      data
+    } = this.props;
+
+    let courses = data.map((course) => {
+      return <Course title={course.title}
+                     desc={course.description}
+                     key={course.id} />
+    });
+    return (
+      <div>
+        <ul>
+          {courses}
+        </ul>
+        <form onSubmit={this.handleAddCourse}>
+        <input type="text" placeholder="Name" ref={ (input) => courses.title = input }/>
+        <input type="text" placeholder="Topic" ref={ (input) => courses.description = input }/>
+        <button type="submit">Go!</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CourseContainer;
